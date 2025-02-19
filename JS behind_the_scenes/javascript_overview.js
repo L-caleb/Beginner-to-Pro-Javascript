c5d6de0eecaaccffc77b95182f4d0988597c991f
l-caleb <lewechicaleb@gmail.com>
2025-02-19 16:03:28 +0100

    note on high level overview in javascript

---
+++ JS behind_the_scenes/javascript_overview.js
@@ -0,0 +1,57 @@
+/*
+JavaScript is a high-level, prototype-based,
+
+object-oriented, multi-paradigm,
+
+interpreted or just-in-time compiled,
+
+dynamic, single-threaded,
+
+garbage-collected programming language
+
+with first-class functions
+
+and a non-blocking event loop concurrency model.
+
+
+Paradigm: an approach and mindset of structuring code, which will direct your coding style and technique
+
+three most popular  paradigm are :
+
+procedural programming : this what we have been doing so far which is basically just organizing the code in a very linear way and with some functions in between
+
+
+object oriented programming: almost everything in js is an object except for primitive values such as numbers strings etc
+
+functional programming
+
+we can also classify paradigm as imperative or declarative
+
+javascript is a language with first class functions : that is functions are treated as regular variables so we can pass functions into other functions and we can even return functions from functions and this allows functional programming
+
+
+Next, I said that JavaScript is a dynamic language
+
+and dynamic actually means dynamically-typed.
+
+So as we've already seen,
+
+in JavaScript, we don't assign data types to variables.
+
+Instead, they only became known
+
+when the JavaScript engine executes our code.
+
+Also, the type of variables can easily be changed
+
+as we reassign variables.
+
+And this is basically what dynamically-typed means.
+
+
+concurrency model: how the js engine handles multiple tasks happeing at the same time
+
+
+
+
+*/
