5da20bc65628a3c2a46af68b4de861a69845ed70
l-caleb <lewechicaleb@gmail.com>
2025-06-12 02:33:48 +0100

    nullish coaliscing operator

---
+++ Data structures,modern_operators_and_strings/Nullish_coalescing_operator.js
@@ -0,0 +1,62 @@
+'use strict';
+
+
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+
+
+  order(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+
+  orderPasta(ing1, ing2, ing3) {
+    console.log(
+      `Here is your declicious pasta with ${ing1}, ${ing2} and ${ing3}`
+    );
+  },
+
+  orderPizza(mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
+};
+
+//NULLISH COALESCING OPERATOR ??
+
+/*
+
+THIS TREATS O AND ' ' AS TRUTHY VALUE
+SO THE NULISH OPERATOR WORKS WITH NULISH VALUES (NULL AND UNDEFINED) AND NOT FALSEY VALUES (0 AND 1)
+Above when we used || operator it sees 0 as falsy and we might want to use 0 as a value but || will assume 0 is falsey so the nullish operator ?? is a solution to that
+
+*/
+
+const guestCorrect = restaurant.numGuests ?? 10;
+console.log(guestCorrect);
+//0
+// this works cause ?? does not see 0 as a falsey value so it sees 0 as truthy and so it returns it
